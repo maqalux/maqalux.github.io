2b18e94f99368a5e6f74247fffcfd23038e95a27
Əliqoşqar Kərimli <145402717+aligoshgar@users.noreply.github.com>
2026-06-23 16:03:04 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// KINOFLIX SMART CHAT — FIREBASE KONFİQURASİYASI
+// ==========================================================================
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkRnRbtDFH9jhJYW4uVKgJVLL5wC0BY08",
+    authDomain: "kinoflixchat.firebaseapp.com",
+    databaseURL: "https://kinoflixchat-default-rtdb.firebaseio.com",
+    projectId: "kinoflixchat",
+    storageBucket: "kinoflixchat.firebasestorage.app",
+    messagingSenderId: "890936189251",
+    appId: "1:890936189251:web:d63fdb1f9ff18021e94ff6",
+    measurementId: "G-BMJYSMJ23B"
+};
